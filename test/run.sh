49f49f1768d98f6e9be3dccf13d45a85550ef348
Florent Clairambault <florent@clairambault.fr>
2022-10-08 20:47:38 +0200

    Updating benchmarks

--- test/run.sh
+++ test/run.sh
@@ -3,7 +3,7 @@
 go test -parallel 20 -v -race -coverprofile=coverage.txt -covermode=atomic -coverpkg=../... ../...
 go tool cover -html=coverage.txt
 
-for test in BenchmarkSmallStruct BenchmarkBiggerStruct BenchmarkThreeRules
+for test in BenchmarkSmallStruct BenchmarkBiggerStruct BenchmarkThreeRules BenchmarkLoadAndExec
 do
   go test -bench=$test -run=^$ -cpuprofile $test.pprof -benchtime=5s
   go tool pprof -svg $test.pprof > $test.svg
